d92287e953d2c0ed3cfdc8c73a7fcb93ddaf7097
Qin Fandong <shell_way@foxmail.com>
2022-05-10 17:58:17 +0800

    更新项目
    1. 添加新角色
    2. 重命名 get-gacha-image.py 至 get_gacha_image.py
    3. 更改了获取图片的流程
    4. 修复了错误的参数处理
    5. 修复了打不开文件浏览器的问题
    6. 添加 FIXME
    7. 更改了日志输出
    8. 更新文档
    9. 更新 .gitignore
    10. 更新 requirements.txt
    11. 使用 black 格式化 .py

--- get_gacha_image.py
+++ get_gacha_image.py
@@ -4,12 +4,23 @@ from lib.APIs import *
 
 
 def get_png(characterName, imagePrefix):
-    fileUrl = "https://genshin.honeyhunterworld.com/img/char/" + imagePrefix + "_gacha_card.png"
+    fileUrl = (
+        "https://genshin.honeyhunterworld.com/img/char/"
+        + imagePrefix
+        + "_gacha_card.png"
+    )
     imagePath = os.path.join(save_path, characterName + ".png")
-    print(characterName, fileUrl)
+    print("获取：{}".format(fileUrl))
+    # FIXME handle exception
     rawBytes = requests.get(fileUrl, headers=headers)
+    # FIXME handle exception
     with open(imagePath, "wb") as f:
         f.write(rawBytes.content)
+    return imagePath
+
+
+def get_webp(characterName, imagePrefix):
+    png_to_webp(get_png(characterName, imagePrefix))
 
 
 if __name__ == "__main__":
@@ -17,24 +28,23 @@ if __name__ == "__main__":
     names_path = os.path.join(wd, "names.yml")
     names = read_config(names_path)
     save_path = os.path.join(wd, "resources_custom", "Version2", "wish", "character")
-
     headers = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36"
     }
+    argc = len(sys.argv)
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
-    if len(sys.argv) >= 1:
-        get_png(sys.argv[1], sys.argv[2])
-    else:
+    if argc > 2:
+        get_webp(*sys.argv[1:3])
+    elif 1 == argc:
         for name, prefix in dict.items(names["characters"]):
-            get_png(name, prefix)
+            get_webp(name, prefix)
+    else:
+        print("参数错误看说明文档")
 
-    for root, dirs, files in os.walk(save_path):
-        for file in files:
-            png_path = os.path.join(root, file)
-            if png_path.endswith(".png"):
-                png_to_webp(png_path)
-
-    open_in_explorer(save_path)
+    try:
+        open_in_explorer(save_path).wait()
+    except Exception as e:
+        print("可以忽略的错误：{}".format(e))
